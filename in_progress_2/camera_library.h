bf5cdc46175fc08bc650f091ccfa4740f0cb5fd8
David Mutchler <mutchler@rose-hulman.edu>
2013-06-18 13:57:19 -0400

    Renamed files so camera_library is current one
    What was camera_library is now camera_library_old.  What was
    camera_library2 is now camera_library.

--- in_progress_2/camera_library.h
+++ in_progress_2/camera_library.h
@@ -1,254 +1,295 @@
-// Created on Sun March 31 2013
-#ifndef _CAMERA_LIBRARY_H_
-#define _CAMERA_LIBRARY_H_
+/*
+ * Camera utilities:
+ *   -- display_blob_numbers
+ *
+ * Written by Aaron and David Mutchler, June 2013.
+ */
 
-#include "lego_library.h"
-//#define LEFT_MOTOR 3
-//#define RIGHT_MOTOR 0
+#ifndef _CAMERA_UTILITIES_H_
+#define _CAMERA_UTILITIES_H_
 
-#define GREEN 0
-#define ORANGE //FIXME
+#include <string.h>
+
+#define NUMBER_OF_BLOBS_TO_SHOW 4
+#define INITIAL_MILLISECONDS_BETWEEN_PICTURES 2000
+#define ERROR_MESSAGE_LINE (2 + NUMBER_OF_BLOBS_TO_SHOW + 1)
+#define BLANK_LINE "                                          "
+#define MAX_ERRORS_ALLOWED_WHEN_OPENING_CAMERA 4
+#define MAX_ERRORS_ALLOWED_BEFORE_FIRST_PICTURE 50
+#define SUCCESS 1
+#define FAILURE 0
 #define LARGEST_BLOB 0
-#define GOOD_ENOUGH 2
+#define LOOP_DELAY 50 // sleep this many milliseconds between camera updates
 
-#define ADJUST_LSPEED 50
-#define ADJUST_RSPEED 30
-#define ADJUST_TIME 50
-#define LOOP_DELAY 20
+// Functions intended to be ** PUBLIC **:
+int initialize_camera(int resolution);
+int move_so_blob_is_at_x(int color_model, int x, int delta, int minimum_area);
+int move_so_blob_is_at_y(int color_model, int y, int delta, int minimum_area);
+void app_to_display_blob_numbers();
+void display_blob_numbers(int color_model, int blob, int header_line, int data_line);
 
-#define SUCKER_MOTOR 2
-#define LIFT_SERVO 1
-#define LIFT_SERVO_DOWN_POSITION 785
-#define LIFT_SERVO_UP_POSITION 2047
-#define LIFT_SERVO_DROP_POSITION 1885
+// Functions intended to be ** PRIVATE **:
+void _initialize_buttons();
+void _display_model_and_resolution(int color_model, int resolution);
 
-#define NO_OF_TIMES_CENTERING 10
+void _take_a_picture();
+void _process_buttons(int* color_model, int* milliseconds_between_pictures);
 
-int DESIRED_Y;
-int DESIRED_X;
+// Functions that must be defined elsewhere for the move... functions to work.
+extern void spin_left_for_camera_search();
+extern void spin_right_for_camera_search();
+extern void move_backwards_for_camera_search();
+extern void move_forwards_for_camera_search();
+extern void stop_camera_search();
 
-point2 get_camera_reading();
-void move_to_y(int y);
-void arm_up();
-void arm_down();
-void suck_up_pom();
-void move_to_x(int x);
-void arm_drop();
-
-point2 get_camera_reading()
-{
-	point2 new;
-	point2 old;
-	int x;
-	old.x = -1;
-	old.y = -1;
-	set_a_button_text("Done");
-	set_b_button_text("Pause");
-	set_c_button_text("Resume");
-	printf("Press 'Done' when the pom is centered\n");
-	msleep(1000);
-	camera_open(LOW_RES);
-	while (!a_button_clicked())
-	{
-		camera_update();
-		x = get_object_count(GREEN);
-		if (x > 0)
-		{
-			new = get_object_center(GREEN, LARGEST_BLOB);
-			if (new.x != old.x || new.y != old.y)
-			{
-				printf("Largest blob at (%i, %i)\n", new.x, new.y);				
-			}
-			old = new;
-		}
-		else
-		{
-			printf("NO VISIBLE BLOB\n");
-		}
-		if (b_button_clicked())
-		{
-			while (1)
-			{
-				if (c_button_clicked())
-				{
-					break;
-				}
-				if (a_button_clicked())
-				{
-					display_clear();
-					printf("Final position is (%i, %i)\n", new.x, new.y);
-					return new;
-				}
-			}
-		}
-	}
-	display_clear();
-	printf("Final position is (%i, %i)\n", new.x, new.y);
-	return new;
-}
-
-void suck_up_pom()
-{
-	int i;
-	motor(SUCKER_MOTOR, 100);
-	extra_buttons_show();
-	for (i = 0; i < 5; i++)
-	{
-		move_to_y(DESIRED_Y);
-		msleep(500);
-		move_to_x(DESIRED_X);
-		msleep(500);
-	}
-	arm_down();
-	sleep(1);
-	arm_up();
-	sleep(2);
-	off(SUCKER_MOTOR);
-}
-
-void arm_down()
-{
-	int position;
-	for (position = get_servo_position(LIFT_SERVO); position > LIFT_SERVO_DOWN_POSITION; position -= 20)
-	{
-		set_servo_position(LIFT_SERVO, position);
-		msleep(20);
-	}
-	off(1);
-}
-
-void arm_up()
-{
-	int position;
-	for (position = get_servo_position(LIFT_SERVO); position < LIFT_SERVO_UP_POSITION; position += 20)
-	{
-		set_servo_position(LIFT_SERVO, position);
-		msleep(20);
-	}
-}
-
-void arm_drop()
-{
-	int position;
-	for (position = get_servo_position(LIFT_SERVO); position < LIFT_SERVO_DROP_POSITION; position += 20)
-	{
-		set_servo_position(LIFT_SERVO, position);
-		msleep(20);
-	}
-}
-// 0 - 159 x
-// 0 - 119 y
-void move_to_y(int y)
-{
-	int center_of_blob;
-	while (1)
-	{
-		camera_update();
-		center_of_blob = get_object_center(GREEN, LARGEST_BLOB).y;
-		display_printf(0, 0, "y, center of blob; %i, %i", y, center_of_blob);
-		if(center_of_blob > y - GOOD_ENOUGH && center_of_blob < y + GOOD_ENOUGH)
-		{
-			
-			display_printf(0, 1, "Robot is centered on blob");
+// Opens the camera at the given resolution.
+// Takes some pictures to be sure that it is working correctly.
+// Returns FAILURE if it cannot open the camera
+// or if picture-taking is not working, else returns SUCCESS.
+// Preconditions:  There must be at least one camera model on the LINK
+//   and it must be able to see at least one blob.
+int initialize_camera(int resolution) {
+	// FIXME: Allow for either camera.
+	// FIXME: Provide a default version that uses LOW_RES.
+	// CONSIDER: What happens if we attempt to open an already-open camera?
+	int status, error_count;
+	
+	printf("Make sure that:\n");
+	printf("  1. The camera is attached, and\n");
+	printf("  2. There is at least one camera model on the LINK, and\n");
+	printf("  3. The camera can see at least one blob after you press A.\n");
+	press_A_to_continue();
+	
+	for (k = 0; k < MAX_ERRORS_ALLOWED_WHEN_OPENING_CAMERA; ++k) {
+		status = camera_open(resolution);
+		if (status == SUCCESS) {
 			break;
 		}
-		else if (center_of_blob < y - GOOD_ENOUGH)
-		{
-			motor(LEFT_MOTOR, ADJUST_LSPEED);
-			motor(RIGHT_MOTOR, ADJUST_RSPEED);
-			msleep(ADJUST_TIME);
-			off(LEFT_MOTOR);
-			off(RIGHT_MOTOR);
-			display_printf(0, 1, "FORWARDS ");
-		}
-		else
-		{
-			motor(LEFT_MOTOR, -ADJUST_LSPEED);
-			motor(RIGHT_MOTOR, -ADJUST_RSPEED);
-			msleep(ADJUST_TIME);
-			off(LEFT_MOTOR);
-			off(RIGHT_MOTOR);
-			display_printf(0, 1, "BACKWARDS");
-		}
+		msleep(500);
+		printf("Warning: Attempt %i to open the camera failed.\n", k + 1);
 	}
-}
-
-
-void move_to_x(int x)
-{
-	int center_of_blob, flag;
-	flag = 1;
-	while (1)
-	{
-		camera_update();
-		center_of_blob = get_object_center(GREEN, LARGEST_BLOB).x;
-		display_printf(0, 0, "x, center of blob; %i, %i", x, center_of_blob);
-		/*if (flag == 1)
-		{
-			if (center_of_blob < x - GOOD_ENOUGH)
-			{
-				motor(LEFT_MOTOR, -100);
-				motor(RIGHT_MOTOR, 100);
-				msleep(70);
-				off(LEFT_MOTOR);
-				off(RIGHT_MOTOR);
-				display_printf(0, 1, "LEFT ");
-			}
-			else
-			{
-				motor(LEFT_MOTOR, 100);
-				motor(RIGHT_MOTOR, -100);
-				msleep(70);
-				off(LEFT_MOTOR);
-				off(RIGHT_MOTOR);
-				display_printf(0, 1, "RIGHT");
-			}
-			flag = -1;
-		}*/
-		if(center_of_blob >= x - GOOD_ENOUGH && center_of_blob <= x + GOOD_ENOUGH)
-		{
-			
-			display_printf(0, 1, "Robot is centered on blob");
+	
+	if (status == FAILURE) {
+		printf("Could not open the camera.\n");
+		printf("Check the 3 things listed above and try again.");
+		return FAILURE;
+	}
+	
+	if (get_channel_count() == 0) {
+		printf("There are no channels in the configuration!\n");
+		printf("Fix your camera configuration.\n");
+		return FAILURE;
+	}
+	// FIXME: Finish this function.
+	/*
+	for (k = 0; k < MAX_ERRORS_ALLOWED_BEFORE_FIRST_PICTURE; ++k) {
+		status = camera_open(resolution);
+		if (status == SUCCESS) {
 			break;
 		}
-		else if (center_of_blob < x - GOOD_ENOUGH)
-		{
-			motor(LEFT_MOTOR, -ADJUST_LSPEED);
-			motor(RIGHT_MOTOR, ADJUST_RSPEED);
-			msleep(ADJUST_TIME);
-			off(LEFT_MOTOR);
-			off(RIGHT_MOTOR);
-			display_printf(0, 1, "LEFT ");
+		msleep(500);
+		printf("Warning: Attempt %i to open the camera failed.\n", k + 1);
+	}*/
+	return SUCCESS;
+}
+
+// Spins the robot left or right until the largest blob of the given color model
+// is within the given delta of the given x.  Considers only blobs whose area is
+// at least the given minimum area.  Returns 1 if it accomplishes the task,
+// or 0 if it does not (that is, if at any point there is no blob visible whose
+// area is at least the given area).
+// Preconditions: The given color model has been set on the Link, x is within
+// the screen dimensions (0 to 120 at LOW_RES), and delta and minimum area are
+// both non-negative.
+int move_so_blob_is_at_x(int color_model, int desired_x, int delta, int minimum_area) {
+	int blob_area, blob_center_x, how_many_times_no_blob_is_visible;
+	
+	how_many_times_no_blob_is_visible = 0;
+	while (1) {
+		// Take a picture and display the numbers for the biggest blob.
+		_take_a_picture();
+		display_blob_numbers(color_model, LARGEST_BLOB, 0, 1);
+		
+		// Is there a big enough blob?
+		// If not, increment error-count and return failure if the count is too big.
+		blob_area = get_object_area(color_model, LARGEST_BLOB);
+		if (blob_area < minimum_area) {
+			++ how_many_times_no_blob_is_visible;
+			if (how_many_times_no_blob_is_visible > MAX_NUMBER_OF_CAMERA_ERRORS) {
+				stop_camera_search();
+				display_printf(0, 2, "FAILURE - I cannot see a big enough blob.");
+				return 0;
+			}
+		} else {
+		
+			// Is the center of the blob within delta of the desired x?
+			// If so, return success.  Otherwise, spin left or right as appropriate.
+			blob_center_x = get_object_center(color_model, LARGEST_BLOB).x;
+		
+			if (blob_center_x >= desired_x - delta && blob_center_x <= desired_x + delta) {
+				stop_camera_search();
+				display_printf(0, 2, "SUCCESS - quit at %i, trying for %i", blob_center_x, desired_x);
+				return 1;
+			} else if (blob_center_x < desired_x - delta) {
+				display_printf(0, 2, "Spin LEFT ");
+				spin_left_for_camera_search();
+			} else {
+				display_printf(0, 2, "Spin RIGHT");
+				spin_right_for_camera_search();
+			}
 		}
-		else
-		{
-			motor(LEFT_MOTOR, ADJUST_LSPEED);
-			motor(RIGHT_MOTOR, -ADJUST_RSPEED);
-			msleep(ADJUST_TIME);
-			off(LEFT_MOTOR);
-			off(RIGHT_MOTOR);
-			display_printf(0, 1, "RIGHT");
-		}
+		
+		// Wait a bit before taking the next picture
 		msleep(LOOP_DELAY);
 	}
 }
 
-void center_green()
-{
-	int center_of_green, r_marker, l_marker;
+int move_so_blob_is_at_y(int color_model, int desired_y, int delta, int minimum_area) {
+	int blob_area, blob_center_y, how_many_times_no_blob_is_visible;
 	
-	while (1)
-	{
-		camera_update();
-		center_of_green = get_object_center(GREEN, LARGEST_BLOB).x;
-		r_marker = get_object_center(ORANGE, LARGEST_BLOB).x;
-		l_marker = get_object_center(ORANGE, LARGEST_BLOB).x;
+	how_many_times_no_blob_is_visible = 0;
+	while (1) {
+		// Take a picture and display the numbers for the biggest blob.
+		_take_a_picture();
+		display_blob_numbers(color_model, LARGEST_BLOB, 0, 1);
+		
+		// Is there a big enough blob?
+		// If not, increment error-count and return failure if the count is too big.
+		blob_area = get_object_area(color_model, LARGEST_BLOB);
+		if (blob_area < minimum_area) {
+			++ how_many_times_no_blob_is_visible;
+			if (how_many_times_no_blob_is_visible > MAX_NUMBER_OF_CAMERA_ERRORS) {
+				stop_camera_search();
+				display_printf(0, 2, "FAILURE - I cannot see a big enough blob.");
+				return 0;
+			}
+			continue;
+		}
+		
+		// Is the center of the blob within delta of the desired x?
+		// If so, return success.  Otherwise, spin left or right as appropriate.
+		blob_center_y = get_object_center(color_model, LARGEST_BLOB).y;
+		
+		if (blob_center_y >= desired_y - delta && blob_center_y <= desired_y + delta) {
+			stop_camera_search();
+			display_printf(0, 2, "SUCCESS - quit at %i, trying for %i", blob_center_y, desired_y);
+			return 1;
+		} else if (blob_center_y < desired_y - delta) {
+			display_printf(0, 2, "Move BACKWARDS");
+			move_backwards_for_camera_search();
+		} else {
+			display_printf(0, 2, "Move FORWARDS ");
+			move_forwards_for_camera_search();
+		}
+		
+		// Wait a bit before taking the next picture
+		msleep(LOOP_DELAY);
 	}
 }
 
+// FIXME - write this spec
+// Preconditions: The camera is open and ...
+void app_to_display_blob_numbers() {
+	int color_model, k, resolution, status, milliseconds_between_pictures;
+	
+	// Starting values of variables the user can change.
+	color_model = 0;
+	resolution = LOW_RES;
+	milliseconds_between_pictures = INITIAL_MILLISECONDS_BETWEEN_PICTURES;
+	
+	_initialize_buttons();
+	_display_model_and_resolution(color_model, resolution);
 
+	while (1) {
+		_take_a_picture();
 
+		for (k = 0; k < NUMBER_OF_BLOBS_TO_SHOW; ++k) {
+			display_blob_numbers(color_model, k, 1, k + 2);
+		}
+		
+		_process_buttons(&color_model, &milliseconds_between_pictures);
+		msleep(milliseconds_between_pictures);
+	}
+}
 
+void _initialize_buttons() {
+	extra_buttons_show();
+	
+	set_a_button_text("Next color mdl");
+	set_b_button_text("+ Refresh rate");
+	set_c_button_text("Pause / resume");
+	set_x_button_text("Prev color mdl");
+	set_y_button_text("- Refresh rate");
+	set_z_button_text("Done");
+}
 
+void _display_model_and_resolution(int color_model, int resolution) {
+	char resolution_string[100];
+	
+    if (resolution == LOW_RES) {
+		strcpy(resolution_string, "160 by 120 (LOW_RES)");
+	} else if (resolution == MED_RES) {
+		strcpy(resolution_string, "320 by 240 (MED_RES)");
+	} else {
+		strcpy(resolution_string, "640 by 480 (HIGH_RES)");
+	}
+	
+	display_printf(0, 0, "Color model %i.  %s", color_model, resolution_string);
+}
+
+// Displays all the statistics available in the KISS library about
+// the given blob (largest, next-largest, ...) in the given color model.
+// Displays the header and data at the given line numbers. 
+void display_blob_numbers(int color_model, int blob, int header_line, int data_line) {
+	display_printf(0, header_line, "B  Center Cntroid Area Cfd ulX ulY lrX lrY");
+	
+	if (get_object_count(color_model) < blob + 1) {
+		display_printf(0, data_line, "%s", BLANK_LINE);
+	} else {
+		display_printf(0, data_line,
+			"%1i %3i %3i %3i %3i %4i %3.1f %3i %3i %3i %3i",
+			blob,
+			get_object_center(color_model, blob).x,
+			get_object_center(color_model, blob).y,
+			get_object_centroid(color_model, blob).x,
+			get_object_centroid(color_model, blob).y,
+			get_object_area(color_model, blob),
+			get_object_confidence(color_model, blob),
+			get_object_bbox(color_model, blob).ulx,
+			get_object_bbox(color_model, blob).uly,
+			get_object_bbox(color_model, blob).ulx + get_object_bbox(color_model, blob).width,
+			get_object_bbox(color_model, blob).uly + get_object_bbox(color_model, blob).height);
+	}
+}
+
+void _take_a_picture() {
+	int status;
+	
+	while (1) {
+		status = camera_update();
+		if (status != 1) {
+			display_printf(0, ERROR_MESSAGE_LINE, "Could not update camera (i.e., take a picture.");
+		} else {
+			display_printf(0, ERROR_MESSAGE_LINE, "%s", BLANK_LINE);
+			break;
+		}
+	}
+}
+
+void _process_buttons(int* color_model, int* milliseconds_between_pictures) {
+	if (a_button()) {
+		display_printf(0, ERROR_MESSAGE_LINE, "Hands off!");
+		
+		if (*color_model >= get_channel_count() - 1) {
+			display_printf(0, ERROR_MESSAGE_LINE, "You are already at the biggest color model!");
+		} else {
+			*color_model = *color_model + 1;
+		}
+		_display_model_and_resolution(*color_model, LOW_RES);
+		
+		while (a_button()) ;   // Wait for user to take her hands off the button
+		display_printf(0, ERROR_MESSAGE_LINE, "%s", BLANK_LINE);
+	}
+}
 
 #endif
